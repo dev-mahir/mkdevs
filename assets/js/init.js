09b5e6133e5004779d4d642898b2e7ea1d515769
mdmahir-mk <mdmahir.ds@gmail.com>
2022-03-01 21:28:46 +0600

    upload

---
+++ assets/js/init.js
@@ -0,0 +1,20 @@
+/* Template Name: MKBIO || Onepage fashion designer template
+   Author: MK Themes
+   Email: support@mkthemes.in
+   Website: https://mkthemes.in
+   Version: 1.0.0
+   Created: February 2022
+   File Description: Init all plugins here
+*/
+
+
+/* ======= Index =========*/
+
+
+
+
+
+// Mixitup filtering plugin 
+
+
+var mixer = mixitup('.filter-container');
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/* Template Name: MKBIO || Onepage fashion designer template
+   Author: MK Themes
+   Email: support@mkthemes.in
+   Website: https://mkthemes.in
+   Version: 1.0.0
+   Created: February 2022
+   File Description: Main JS file of the template
+*/
+
